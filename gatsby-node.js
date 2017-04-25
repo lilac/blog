2485f7802cf03f2ca2f233d5bfff1ccb8ca9210a
Junjun Deng <cnjamesdeng@gmail.com>
2017-04-25 21:00:22 +0800

    Add file for google site verification

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,7 @@
 const rucksack = require('rucksack-css');
 const lost = require('lost');
 const cssnext = require('postcss-cssnext');
+const fs = require("fs-extra");
 
 exports.modifyWebpackConfig = function (config) {
   config.merge({
@@ -20,3 +21,15 @@ exports.modifyWebpackConfig = function (config) {
 
   return config;
 };
+
+exports.postBuild = () => {
+  let filename = 'google7e55f41597bad74b.html'
+  fs.copySync(
+    `./${filename}`,
+    `./public/${filename}`
+  )
+  /* Alternative
+  import Shell from 'child_process'
+  Shell.execSync("cp -r assets/* public/")
+  */
+}
