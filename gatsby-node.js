655d27ed569de6b7a73b9dc52124555fe348538a
Junjun Deng <cnjamesdeng@gmail.com>
2017-05-30 21:10:06 +0800

    Generate sitemap post building

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,6 +2,35 @@ const rucksack = require('rucksack-css');
 const lost = require('lost');
 const cssnext = require('postcss-cssnext');
 const fs = require("fs-extra");
+const sm = require('sitemap');
+// const config = require('config');
+
+function pagesToSitemap(pages) {
+  const urls = pages.map((p) => {
+    if (p.path !== undefined) {
+      return {
+        url: p.path,
+        changefreq: 'daily',
+        priority: 0.7
+      }
+    }
+  })
+  // remove undefined (template pages)
+  return urls.filter(u => u !== undefined)
+}
+
+function generateSiteMap(pages) {
+  const sitemap = sm.createSitemap({
+    hostname: 'https://blog.samemoment.com', //config.baseUrl,
+    cacheTime: '60000',
+    urls: pagesToSitemap(pages),
+  })
+  console.log('Generating sitemap.xml')
+  fs.writeFileSync(
+    `${__dirname}/public/sitemap.xml`,
+    sitemap.toString()
+  )
+}
 
 exports.modifyWebpackConfig = function (config) {
   config.merge({
@@ -22,7 +51,10 @@ exports.modifyWebpackConfig = function (config) {
   return config;
 };
 
-exports.postBuild = () => {
+exports.postBuild = (pages, callback) => {
   const Shell = require('child_process');
-  Shell.execSync("cp -r assets/* public/")
+  console.log('Copying assets to public dir.');
+  Shell.execSync("cp -r assets/* public/");
+  generateSiteMap(pages);
+  callback();
 }
